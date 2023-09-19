0c16715a174d023f74113a8efeddd3fd73332e51
Sergio <sergiop31.sp@gmail.com>
2023-09-19 16:18:48 +0200

    first commit

---
+++ src/main/java/com/projectweek2/PROJECT_WEEK_2/ProjectWeek2Application.java
@@ -0,0 +1,13 @@
+package com.projectweek2.PROJECT_WEEK_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWeek2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectweek2/PROJECT_WEEK_2/ProjectWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectweek2.PROJECT_WEEK_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
